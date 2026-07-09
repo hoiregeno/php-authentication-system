c00e2b19dbff339c0071a25727e4a4e05b4b0089
hoiregeno <hoiregeno75@gmail.com>
2026-07-09 12:19:03 +1000

    chore(database): establish database connection

--- db.php
+++ db.php
@@ -1,2 +1,15 @@
 <?php
-  
+  $hostname = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "krusty_krabs";
+  $err_message = "";
+
+  mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+  try{
+    $conn = mysqli_connect($hostname, $username, $password, $dbname);
+  }
+  catch(mysqli_sql_exception $e){
+    die("Database connection failed" . $e->getMessage());
+  }
